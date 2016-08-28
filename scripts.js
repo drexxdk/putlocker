4ba2452cf1714b1f3362f6e0a8bb8d744bd1c3be
draxxdk <frederik@symmetric.dk>
2016-08-28 12:07:18 +0200

    x

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
-﻿$(function () {
+﻿// challenges with site:
+// - table layout
+// - most elements doesn't have id
+// - only way to identity one element from another is by looking at ancestors and siblings
+$(function () {
     var body = $("body");
     var page =
         '<div id="main">' +
@@ -21,7 +25,7 @@
 
     var content = $("body > table:nth-of-type(2) > tbody > tr > td:first-child .content-box");
 
-    // video
+    // movie / tv-show
     if (body.children().first().attr("id") === "fb-root") {
         body.attr("id", "video");
         $("#content").append(
@@ -29,33 +33,60 @@
                 '<div id="info"></div>' +
                 '<div id="links"></div>' + 
             '</div>' +
-            '<div id="right"></div>'
+            '<div id="right">' +
+                '<div id="embed"></div>' +
+            '</div>'
             );
         var left = $("#left");
         var right = $("#right");
         var h1 = content.find("h1");
         var info = content.find("h1 + table > tbody > tr > td:last-child > table");
 
-        var embed = content.find("h1 + table + h2 + table");
-        var links_1 = content.find("#MarketGidScriptRootC9737 + h2 + table");
-        var links_2 = content.find("#MarketGidScriptRootC9737 + h2 + table + table");
-
-        h1.appendTo(right);
         info.appendTo(left.children("#info"));
-        right.append('<div id="embed"></div>');
-        embed.appendTo($("#embed"));
+        h1.prependTo(right);
+        var isTvShow = content.children().eq(1).hasClass("addthis_toolbox");
+        if (isTvShow) {
+            var started = false;
+            content.children().each(function (index, value) {
+                var ignoreLast = $(value).next().attr("id") === "MarketGidScriptRootC9737";
+                if (ignoreLast) {
+                    return false;
+                }
+                if(started) {
+                    $(value).appendTo(left.children("#links"));
+                }
+                var ignoreFirst = $(value).hasClass("addthis_toolbox");
+                if(ignoreFirst) {
+                    started = true;
+                }
+            });
+        } else {
+            var embed = content.find("table:first-of-type + h2 + table");
+            var links_1 = content.find("#MarketGidScriptRootC9737 + h2 + table");
+            var links_2 = content.find("#MarketGidScriptRootC9737 + h2 + table + table");
 
-        links_1.appendTo(left.children("#links"));
-        links_2.appendTo(left.children("#links"));
+            embed.appendTo($("#embed"));
+
+            links_1.appendTo(left.children("#links"));
+            links_2.appendTo(left.children("#links"));
+
+            
+        }
 
         left.children("#links").find("table > tbody > tr > td:nth-child(2)").each(function () {
             var $this = $(this);
             var link = $this.children("a");
-            var provider = $this.html().substring($this.html().lastIndexOf(";") + 1).trim();
-            link.text(provider);
+            var provider = "<span>" + $this.html().substring($this.html().lastIndexOf(";") + 1).trim() + "</span>";
+            
+
+            if (isTvShow) {
+                var episode = "<span>" + link.text() + "</span>";
+                link.html(episode + provider);
+            } else {
+                link.html(provider);
+            }
             $this.html(link);
         });
-
     } else {
         body.attr("id", "browse");
         $(content).appendTo("#content");
